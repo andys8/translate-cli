a30f12fdbde3b3d378d04e5cbe7b64cfae634022
andys8 <andys8@users.noreply.github.com>
2017-10-14 21:46:19 +0200

    Using argument for api

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,19 +5,22 @@ module Lib(translateText) where
 
 import Network.Wreq
 import Control.Lens ((&), (^.), (^?), (.~), (^..))
-import Data.Text (Text)
+import Data.Text (Text, pack)
 import GHC.Generics (Generic)
-import Data.Aeson (decode, FromJSON)
+import Data.Aeson 
 import Data.Aeson.Lens (key)
 
 import Model
 
-instance FromJSON Model.RestPhrase
-instance FromJSON Model.RestResult
-instance FromJSON Model.RestResponse
+instance FromJSON RestPhrase
+instance FromJSON RestResult
+instance FromJSON RestResponse
 
 translateText :: String -> IO()
-translateText _ = apiRequest
+translateText phrase = do
+    r <- getWith (getOptions phrase) $ apiBase
+    let dr = eitherDecode (r ^. responseBody) :: Either String RestResponse
+    print dr
 
 apiBase :: String
 apiBase = "https://glosbe.com/gapi_v0_1/translate"
@@ -30,10 +33,12 @@ requestParams :: String
 requestParams = "?from=de&dest=eng&format=json&phrase=haus"
 
 
-apiRequest :: IO ()
-apiRequest = do
-    let opts = defaults & param "foo" .~ ["bar"]
-    --r <- getWith opts apiBase
-    r <- get $ apiBase ++ requestParams
-    --putStrLn $ show (r ^. responseBody)
-    putStrLn $ show $ (decode ((r ^. responseBody)) :: Maybe Model.RestResponse)
+getOptions :: String -> Options
+getOptions phrase =
+    let phraseText = pack phrase
+    in defaults
+        & param "phrase" .~ [phraseText]
+        & param "from" .~ ["de"]
+        & param "dest" .~ ["en"]
+        & param "format" .~ ["json"]
+
--- src/Model.hs
+++ src/Model.hs
@@ -4,20 +4,15 @@ module Model(RestResponse, RestResult, RestPhrase) where
 import Data.Text (Text)
 import GHC.Generics (Generic)
 
-data Translation = Translation { translation :: String
-                               } deriving (Show, Generic)
 
--- Rest Api
-
-data RestPhrase = RestPhrase { text :: Text
-                             , language :: Text
+data RestPhrase = RestPhrase { text :: String
+                             , language :: String
                              } deriving (Generic, Show)
 
-data RestResult = RestResult { meaningId :: Integer
-                             , phrase :: RestPhrase
+data RestResult = RestResult { phrase :: Maybe RestPhrase
                              } deriving (Generic, Show)
 
-data RestResponse = RestResponse { result :: !Text
+data RestResponse = RestResponse { result :: String
                                  , tuc :: [RestResult]
                                  } deriving (Generic, Show)
 
