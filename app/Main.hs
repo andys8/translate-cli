b4bfc5e2ba786398a74a821f83dc5a232c43d186
andys8 <andys8@users.noreply.github.com>
2017-10-14 15:59:20 +0200

    COmmand line parser showing version

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Turtle
 import Lib
+import Paths_translate_cli (version)
+import Data.Version (showVersion)
+
+--main :: IO ()
+--main = someFunc
+
+
+mainSubroutine :: IO ()
+mainSubroutine = echo "Any tool just works!"
+
+parseMain :: Parser (IO ())
+parseMain = pure mainSubroutine
+
+parseVersion :: Parser (IO ())
+parseVersion =
+    (subcommand "version" "Show version information" (pure version'))
+
+parser :: Parser (IO ())
+parser = parseMain <|> parseVersion
 
 main :: IO ()
-main = someFunc
+main = do
+    cmd <- options "Translation tool for the commandline" parser
+    cmd
+
+version' :: IO()
+version' = putStrLn (showVersion version)
+
